68643c3da1f8c5e0c887c5e0c3a7cb4f9a188c7b
KIMTAESEONG <aka404365@gmail.com>
2023-12-14 16:06:16 +0900

    feat: editted global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import Link from 'next/link'
 import { Inter } from 'next/font/google'
 import './globals.css'
-
+import Providers from './components/Providers'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
@@ -20,21 +20,6 @@ export default function RootLayout({
             <Link href="/" className="text-3xl font-bold text-blue-400">
               똑똑한 청년.
             </Link>
-            <div className="flex justify-center items-center gap-2">
-              <Link
-                href="/login"
-                className="flex flex-col justify-end text-base font-semibold"
-              >
-                로그인
-              </Link>
-              |
-              <Link
-                href="/signup"
-                className="flex flex-col justify-end text-base font-semibold"
-              >
-                회원가입
-              </Link>
-            </div>
           </div>
 
           <div className="flex gap-4 w-full md:w-4/5 lg:w-1/2">
@@ -46,7 +31,7 @@ export default function RootLayout({
           </div>
 
           <div className="border p-6 rounded-2xl shadow-lg bg-slate-50 w-full h-3/4 md:w-4/5 md:h-3/4 lg:w-1/2 lg:h-3/4">
-            {children}
+            <Providers>{children}</Providers>
           </div>
         </div>
       </body>
